23d9445641333f3afd5996fe993591b3f3ffd5fc
Agron Gemajli (UConn) <agron.gemajli@uconn.edu>
2023-01-25 20:50:09 -0500

    added some edge cases

--- week2/PrefixSum.cpp
+++ week2/PrefixSum.cpp
@@ -4,10 +4,16 @@ int prefix_sum(int array[], int length){
     //returns the sum of the array up to index length.
 
     int temp = 0;
-    //case where we ask it to return prefix sum of length 0.
+ //case where no length
     if(length == 0){
+        return 0;
+    }
+
+    //case where we ask it to return prefix sum of length 1.
+    if(length == 1){
         return array[0];
     }
+
 
     //up to the length, just sum every value
     for(int i = 0; i < length; i++){
@@ -21,6 +27,9 @@ int prefix_sum(int array[], int length){
 //This function checks that prefix sum at every length is non negative.
 //if at least 1 prefix sum is < 0, return false.
 bool non_negative_sum(int array[], int length){
+    if(length == 0) {
+        return true;
+    }
     for(int i = 0; i < length; i++){
         if(prefix_sum(array, i) < 0) return false;
     }
@@ -30,6 +39,9 @@ bool non_negative_sum(int array[], int length){
 //This function checks that prefix sum at every length is non positive
 //if at least 1 prefix sum is >0, return false.
 bool non_positive_sum(int array[], int length){
+    if(length == 0) {
+        return true;
+    }
     for(int i = 0; i < length; i++){
         if(prefix_sum(array, i) > 0) return false;
     }
--- week2/UnitTestSum.cpp
+++ week2/UnitTestSum.cpp
@@ -10,6 +10,10 @@ int nums_3[] = {-1, -1, 1, 1}; //none are positive. last one is 0.
 int nums_4[] = {5, 10, 15, 20}; //prefix sum should be {5, 15, 30, 50} all positive
 int nums_5[] = {-5, -10, -15, -20}; //prefix sum should be {-5. -15, -30, -50} // all negative
 
+int nums_6[] = {};
+int nums_7[] = {25};
+int nums_8[] = {-25};
+
 //all prefix sums should return 0 if successful
 TEST_CASE("testing the actual prefix sum functions") {
     CHECK(prefix_sum(nums_1, sizeof(nums_1)/sizeof(nums_1[0])) == 4);
@@ -17,6 +21,9 @@ TEST_CASE("testing the actual prefix sum functions") {
     CHECK(prefix_sum(nums_3, sizeof(nums_3)/sizeof(nums_3[0])) == 0);
     CHECK(prefix_sum(nums_4, sizeof(nums_4)/sizeof(nums_4[0])) == 50);
     CHECK(prefix_sum(nums_5, sizeof(nums_5)/sizeof(nums_5[0])) == -50);
+    CHECK(prefix_sum(nums_6, sizeof(nums_6)/sizeof(nums_6[0])) == 0);
+    CHECK(prefix_sum(nums_7, sizeof(nums_7)/sizeof(nums_7[0])) == 25);
+    CHECK(prefix_sum(nums_8, sizeof(nums_8)/sizeof(nums_8[0])) == -25);
 }
 
 TEST_CASE("testing the sums function modifications") {
@@ -25,4 +32,7 @@ TEST_CASE("testing the sums function modifications") {
     CHECK(non_positive_sum(nums_3, sizeof(nums_3)/sizeof(nums_3[0])) == true);
     CHECK(non_positive_sum(nums_4, sizeof(nums_4)/sizeof(nums_4[0])) == false);
     CHECK(non_positive_sum(nums_5, sizeof(nums_5)/sizeof(nums_5[0])) == true);
+    CHECK(non_negative_sum(nums_7, sizeof(nums_7)/sizeof(nums_7[0])) == true);
+    CHECK(non_positive_sum(nums_8, sizeof(nums_8)/sizeof(nums_8[0])) == true);
+
 }
