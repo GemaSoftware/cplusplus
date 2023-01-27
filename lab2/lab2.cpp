333162106a51377ade753e5d300d7933b896aed8
Agron Gemajli (UConn) <agron.gemajli@uconn.edu>
2023-01-27 17:34:32 -0500

    turned main into functions to start unitesting

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -1,9 +1,10 @@
-#include "../week2/PrefixSum.h"
 #include "lab2.h"
 #include <iostream>
 
 
 using namespace std;
+
+//Function Implementations
 
 void generate_num_list(int array[], int length){
     //first half should be 1's, second should be 0's
@@ -25,33 +26,45 @@ void fisher_yates_scramble(int array[], int length){
         array[j] = temp;
     }
 }
+ 
+bool check_well_balanced_list(int array[], int length) {
+    //After analysis, if the array is itself a non-negative prefix sum, it is a well-balanced list. As the prefix sum will never go below 0. 
+    //Acts like a stack.
+
+    if(non_negative_sum(array, length) == true) return true;
+    else return false;
+}
+
+int well_balanced_list_gen(int n, int times) {
+    int success = 0;
+    for(int i = 0; i < times; i++){
+        //init array of 2n integers
+        int array[2*n];
+
+        //fill the array with 1's and -1s
+        generate_num_list(array, 2*n);
+
+        //call the fisher yates algorithm and scramble the list randomly.
+        fisher_yates_scramble(array, 2*n);
+
+        //check if the array is successful
+        bool check = check_well_balanced_list(array, 2*n);
+
+        if(check == true) success += 1;
+
+    }
+    return success;
+}
+
 
 int main(){
-    //this is where we will do basic testing.
-    int n;
-    cin >> n; 
-    int att;
-    cin >> att;
-
-    int success = 0;
-
-    for(int i = 0; i < att; i++){
-        int array[n];
-
-    //generate the list of numbers that are 1's and -1's
-    generate_num_list(array, n);
-
-    //call the fisher yates algorithm on the array.
-    fisher_yates_scramble(array, n);
-
-
-    if(non_negative_sum(array, n) == true){
-        success+=1;
-    }
-    }
-
-    cout << "successes out of " << att << " attempts is " << success << endl;
-
+    //Will create Unit Testing for each function in another file. This is mainly to check for the convergence.
+    //So with n=30, we will run the algorithm at least 10000 times.
+    cout << "Successes (n=30, att=1000): " << well_balanced_list_gen(30, 1000) << endl;
+    cout << "Successes (n=30, att=2000): " << well_balanced_list_gen(30, 2000) << endl;
+    cout << "Successes (n=30, att=4000): " << well_balanced_list_gen(30, 4000) << endl;
+    cout << "Successes (n=30, att=8000): " << well_balanced_list_gen(30, 8000) << endl;
+    cout << "Successes (n=30, att=16000): " << well_balanced_list_gen(30, 16000) << endl;
 
     return 0;
-}
+} 
--- lab2/lab2.h
+++ lab2/lab2.h
@@ -1,7 +1,30 @@
+#include "PrefixSum.h"
+
 #ifndef __PREFIX_SUM_H
 #define __PREFIX_SUM_H
+/// @brief Takes the array provided and fills hald the array with 1's and half the array with -1's.
+/// @param array newly initialized array.
+/// @param length length of array
+/// @return void. However, the array will have been modified to half 1 and half -1
+void generate_num_list(int array[], int length);
 
-void generate_num_list(int length);
+
+/// @brief Scrambles the array via the fisher yates algorithm.
+/// @param array array containing the integers to scramble
+/// @param length the length of the array.
 void fisher_yates_scramble(int array[], int length);
+
+
+/// @brief This function will return the number of successful well balanced lists given 1000 iterations of random lists of 2n.
+/// @param n integer n that will be used to generate list of 2n matching symbols.
+/// @param times the number of times to generate, scramble, and check for well-balanced list
+/// @return The number of successful well balanced lists from 1000 attempts for 2n symbols.
+int well_balanced_list_gen(int n, int times);
+
+/// @brief Checks to make sure the array is a "well-balanced list"
+/// @param array array to check well balanced list
+/// @param length length of array
+/// @return true if the list is well balanced. False otherwise.
+bool check_well_balanced_list(int array[], int length);
 
 #endif
