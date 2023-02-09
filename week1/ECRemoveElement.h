507dc734e58a37015211b4f82a761536bbe1e3c1
Agron Gemajli (UConn) <agron.gemajli@uconn.edu>
2023-02-08 21:49:13 -0500

    Fixed week1 items

---
+++ week1/ECRemoveElement.h
@@ -0,0 +1,4 @@
+#ifndef __WEEK1_FUNCS
+#define __WEEK1_FUNCS
+void ECRemoveSpace(int nums[], int len, int val);
+#endif
--- week1/TestNumbers.cpp
+++ week1/TestNumbers.cpp
@@ -1,8 +1,9 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <iostream>
 #include "ECNumbers.h"
 #include "ECCalculator.h"
+#include "ECRemoveElement.h"
 #include "../doctest/doctest.h"
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 
 using namespace std;
 
@@ -18,4 +19,20 @@ TEST_CASE("testing the double functions") {
     CHECK(ECNumbers::ECDoubleN(4) == 8);
     CHECK(ECNumbers::ECDoubleN(5) == 10);
     CHECK(ECNumbers::ECDoubleN(2) == 4);
+}
+
+TEST_CASE("Testing the ECRemoveSpace functions"){
+    int nums1[] = {3,2,2,3};
+    int nums2[] = {0,1,2,2,3,0,4,2};
+
+    ECRemoveSpace(nums1, 4, 3);
+    CHECK(nums1[0] == 2);
+    CHECK(nums1[1] == 2);
+
+    ECRemoveSpace(nums2, 8, 2);
+    CHECK(nums2[0] == 0);
+    CHECK(nums2[1] == 1);
+    CHECK(nums2[2] == 3);
+    CHECK(nums2[3] == 0);
+    CHECK(nums2[4] == 4);
 }
