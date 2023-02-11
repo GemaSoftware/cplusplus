2cadb7d8baf1afee79b8628ee07f91678a3ad418
Agron Gemajli (UConn) <agron.gemajli@uconn.edu>
2023-02-11 12:08:07 -0500

    added stack and queue and exceptions

--- week4/TestWeek4.cpp
+++ week4/TestWeek4.cpp
@@ -4,13 +4,40 @@
 
 //BEGIN UNIT TESTS
 TEST_CASE("Testing Stack Functions") {
-    
-    SUBCASE(""){
 
+    SUBCASE("Testing base stack push/pop functions"){
+        std::string filename = "posSum";
+        std::stack<int> stack;
+        pushElementsToStlStack(stack, filename);
+        //CHECK LEN = 10
+        CHECK(stack.size() == 10);
+        CHECK(stack.top() == 9);
+        //CHECK POP
+        for(int i = 0; i < 10; i++){
+            CHECK(stack.size() == 10-i);
+            CHECK(popElementStlStack(stack) == 10-i-1);
+        }
+        //CHECK stack is empty;
+        CHECK(stack.empty() == true);
     }
 
-    SUBCASE("") {
-        
+    SUBCASE("testing the negative sum exceptions"){
+        std::string filename = "negSum";
+        std::stack<int> test;
+        CHECK_THROWS(pushElementsToStlStack(test, filename));
+    }
+
+    SUBCASE("testing the queue functions") {
+        std::string filename = "posSum";
+        std::queue<int> queue;
+        queueElementsStlQueue(queue, filename);
+        CHECK(queue.size() == 10);
+        CHECK(queue.front() == 0);
+        for(int i = 0; i<10; i++){
+            CHECK(dequeueElementStlQueue(queue) == i);
+        }
+        CHECK(queue.size() == 0);
+        CHECK(queue.empty() == true);
     }
 
 }
--- week4/week4funcs.cpp
+++ week4/week4funcs.cpp
@@ -1,5 +1,4 @@
 #include "week4funcs.h"
-#include <fstream>
 
 // Using the STL, write C++ functions for pushing elements to an STL stack of structs and popping these structs.
 
@@ -9,33 +8,49 @@
 
 // If the sum of the elements on a stack have a negative sum, then an exception must be thrown. You must
 // also catch that exception.
+using namespace std;
 
-void pushElementStlStack(std::stack<int> stack, const std::string& filename)
+void pushElementsToStlStack(std::stack<int>& stack, const std::string& filename)
 {
     //Check if the file is open.
     std::ifstream file(filename);
-    if(file.is_open()){
+
+    int sum = 0;
+    try{
+        if(file.is_open()){
         int x = 0;
         //if file open, loop through each value and push into the stack.
         while(file >> x){
-            stack.push(x);
+            stack.push(int(x));
+            sum += (int) x;
+        }
+        //if sum negative, throw exception.
+        if(sum < 0){
+                throw std::string("negative sum");
         }
     }
     // close file after reading everything.
     file.close();
+    } catch(std::string){ //if exception caught, negative sum , close file.
+        //close the file.
+        file.close();
+        //throw it back to <.>_<.>
+        throw;
+    }
+    
 }
 
-int popElementStlStack(std::stack<int> stack)
+int popElementStlStack(std::stack<int>& stack)
 {
     if(!stack.empty()){
         int temp =  stack.top();
         stack.pop();
         return temp;
     }
-    return ;
+    return 0;
 }
 
-void queueElementsStlQueue(std::queue<int> queue, std::string &filename)
+void queueElementsStlQueue(std::queue<int>& queue, std::string &filename)
 {
     // Will be almost identical to stack. 
     // Check if the file is open.
@@ -51,7 +66,7 @@ void queueElementsStlQueue(std::queue<int> queue, std::string &filename)
     file.close();
 }
 
-int dequeueElementStlQueue(std::queue<int> queue)
+int dequeueElementStlQueue(std::queue<int>& queue)
 {
      if(!queue.empty()){
         int temp = queue.front();
