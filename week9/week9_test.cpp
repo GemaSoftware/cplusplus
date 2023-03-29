ffbbb9a499c76b5a864a3c3e4d2841aaaa72cacd
Agron Gemajli (UConn) <agron.gemajli@uconn.edu>
2023-03-28 20:43:46 -0400

    Initial commit. Final Code

---
+++ week9/week9_test.cpp
@@ -0,0 +1,15 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "../doctest/doctest.h"
+#include "rllptr.h"
+
+TEST_CASE("Testing creating the List struct and its functions"){
+    List* x = new List();
+    x->push(1);
+    x->push(2);
+    x->push(3);
+    CHECK(x->head->data == 3);
+    CHECK(x->head->next->data == 2);
+    CHECK(x->head->next->next->data == 1);
+    delete x;
+
+}
